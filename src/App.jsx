9ed32160f6f9f24ffb537913bb6de100aa6c717f
kristban <kristinbannasch@hotmail.com>
2025-05-21 06:37:22 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  
-return <div className="App">Weather App</div>
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Kristin and is{" "}
+        <a href="https://github.com/kristban/final-weather-app" target="blank">open-sourced on Github</a>
+      </footer>
+    </div>
+  )
 }
 
-export default App
