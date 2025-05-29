4bfb9e18c8229a16e9d5d6f1f612ed4a2093f238
kristban <kristinbannasch@hotmail.com>
2025-05-29 21:59:46 +0100

    added html to weather componant and started working on CSS

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -3,6 +3,41 @@ import './Weather.css'
 
 export default function Weather(){
     return (
-        <div className='weather'>Hello from weather</div>
+        <div className='weather'>
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input 
+                            type="search" 
+                            placeholder="Enter your City..." 
+                            className="form-control"/>
+                    </div>
+                    <div className="col-3">
+                        <input 
+                            type="submit" 
+                            value="Search" 
+                            className="btn btn-primary"/>
+                    </div>
+                </div>
+            </form>
+            <h1>Cork</h1>
+            <ul>
+                <li>Friday 09:00</li>
+                <li>Mostly cloudy</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Mostly cloudy" />
+                    14 C|F
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Percipitation: 15%</li>
+                        <li>Humidity: 72%</li>
+                        <li>Wind: 13 km/h</li>
+                    </ul>
+                </div>
+            </div>
+            </div>
     )
 }
